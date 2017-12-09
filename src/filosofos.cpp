dd454438169ba668e941531e2e1f3aee8e6bd9da
aron <pedrobonillanadal@hotmail.com>
2017-12-10 00:37:20 +0100

    acabando la practica

--- src/filosofos.cpp
+++ src/filosofos.cpp
@@ -17,14 +17,15 @@
 #include <random> // dispositivos, generadores y distribuciones aleatorias
 #include <chrono> // duraciones (duration), unidades de tiempo
 #include <iostream>
+#include <utility>// pair
 
 using namespace std;
 using namespace std::this_thread ;
 using namespace std::chrono ;
 
 const int
-   num_filosofos = 5 ,
-   num_procesos  = 2*num_filosofos ;
+num_filosofos = 5 ,
+  num_procesos  = 2*num_filosofos ;
 
 
 //**********************************************************************
@@ -45,45 +46,40 @@ template< int min, int max > int aleatorio()
 void funcion_filosofos( int id )
 {
   int id_ten_izq = (id+1)              % num_procesos, //id. tenedor izq.
-      id_ten_der = (id+num_procesos-1) % num_procesos; //id. tenedor der.
+    id_ten_der = (id+num_procesos-1) % num_procesos; //id. tenedor der.
 
   int peticion=0;
  
   while ( true )
-  {
+    {
     
-    cout <<"Filósofo " <<id << " solicita ten. izq." <<id_ten_izq <<endl;
-    MPI_Ssend( &peticion,    1, MPI_INT, id_ten_izq, 0, MPI_COMM_WORLD);
+      cout <<"Filósofo " <<id << " solicita ten. izq." <<id_ten_izq <<endl;
+      MPI_Ssend( &peticion,    1, MPI_INT, id_ten_izq, 0, MPI_COMM_WORLD);
 
-    cout <<"Filósofo " <<id <<" solicita ten. der." <<id_ten_der <<endl;
-    MPI_Ssend( &peticion,    1, MPI_INT, id_ten_der, 0, MPI_COMM_WORLD);
+      cout <<"Filósofo " <<id <<" solicita ten. der." <<id_ten_der <<endl;
+      MPI_Ssend( &peticion,    1, MPI_INT, id_ten_der, 0, MPI_COMM_WORLD);
 
-    cout <<"Filósofo " <<id <<" comienza a comer" <<endl ;
-    sleep_for( milliseconds( aleatorio<10,100>() ) );
+      cout <<"Filósofo " <<id <<" comienza a comer" <<endl ;
+      sleep_for( milliseconds( aleatorio<10,100>() ) );
+      
+      cout <<"Filósofo " <<id <<" suelta ten. izq. " <<id_ten_izq <<endl;
+      MPI_Ssend( &peticion,    1, MPI_INT, id_ten_izq, 0, MPI_COMM_WORLD);
 
-    cout <<"Filósofo " <<id <<" suelta ten. izq. " <<id_ten_izq <<endl;
-    MPI_Ssend( &peticion,    1, MPI_INT, id_ten_izq, 0, MPI_COMM_WORLD);
+      cout<< "Filósofo " <<id <<" suelta ten. der. " <<id_ten_der <<endl;
+      MPI_Ssend( &peticion,    1, MPI_INT, id_ten_der, 0, MPI_COMM_WORLD);
 
-    cout<< "Filósofo " <<id <<" suelta ten. der. " <<id_ten_der <<endl;
-    MPI_Ssend( &peticion,    1, MPI_INT, id_ten_der, 0, MPI_COMM_WORLD);
-
-    cout << "Filosofo " << id << " comienza a pensar" << endl;
-    sleep_for( milliseconds( aleatorio<10,100>() ) );
- }
+      cout << "Filosofo " << id << " comienza a pensar" << endl;
+      sleep_for( milliseconds( aleatorio<10,100>() ) );
+    }
 }
 // ---------------------------------------------------------------------
 
-int orden (int n) {
-  int ret;
-  
-  if (n%2)                  //si es impar
-    ret = n-1;
-  else if (n == num_filosofos-1)
-    ret =  n-1;
-  else
-    ret = n;
+pair<int,int> index (int n) {
 
-  return ret;
+  if ((n/2)%2)         //si el truncamiento de su mitad es impar
+    return pair<int,int> (n-1,(n+1)%(num_filosofos*2));
+  else
+    return pair<int,int> ((n+1)%(num_filosofos*2),n-1);
 }
 
 void funcion_tenedores( int id )
@@ -92,45 +88,54 @@ void funcion_tenedores( int id )
   MPI_Status estado ;       // metadatos de las dos recepciones
 
   while ( true )
-  {
-      MPI_Recv ( &valor, 1, MPI_INT, funcion, 0, MPI_COMM_WORLD, &estado );
+    {
+      MPI_Recv ( &valor, 1, MPI_INT, index( id ).first , 0, MPI_COMM_WORLD, &estado );
       id_filosofo = estado.MPI_SOURCE;// ...... guardar en 'id_filosofo' el id. del emisor (completar)
-     cout <<"Ten. " <<id <<" ha sido cogido por filo. " <<id_filosofo <<endl;
+      cout <<"Ten. " <<id <<" ha sido cogido por filo. " <<id_filosofo <<endl;
 
       MPI_Recv ( &valor, 1, MPI_INT, id_filosofo, 0, MPI_COMM_WORLD, &estado );
-     cout <<"Ten. "<< id<< " ha sido liberado por filo. " <<id_filosofo <<endl ;
-  }
+      cout <<"Ten. "<< id<< " ha sido liberado por filo. " <<id_filosofo <<endl ;
+
+      
+      MPI_Recv ( &valor, 1, MPI_INT, index( id ).second , 0, MPI_COMM_WORLD, &estado );
+      id_filosofo = estado.MPI_SOURCE;// ...... guardar en 'id_filosofo' el id. del emisor (completar)
+      cout <<"Ten. " <<id <<" ha sido cogido por filo. " <<id_filosofo <<endl;
+
+      MPI_Recv ( &valor, 1, MPI_INT, id_filosofo, 0, MPI_COMM_WORLD, &estado );
+      cout <<"Ten. "<< id<< " ha sido liberado por filo. " <<id_filosofo <<endl ;
+
+    }
 }
 // ---------------------------------------------------------------------
 
 int main( int argc, char** argv )
 {
-   int id_propio, num_procesos_actual ;
+  int id_propio, num_procesos_actual ;
 
-   MPI_Init( &argc, &argv );
-   MPI_Comm_rank( MPI_COMM_WORLD, &id_propio );
-   MPI_Comm_size( MPI_COMM_WORLD, &num_procesos_actual );
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank( MPI_COMM_WORLD, &id_propio );
+  MPI_Comm_size( MPI_COMM_WORLD, &num_procesos_actual );
 
 
-   if ( num_procesos == num_procesos_actual )
-   {
+  if ( num_procesos == num_procesos_actual )
+    {
       // ejecutar la función correspondiente a 'id_propio'
       if ( id_propio % 2 == 0 )          // si es par
-         funcion_filosofos( id_propio ); //   es un filósofo
+	funcion_filosofos( id_propio ); //   es un filósofo
       else                               // si es impar
-         funcion_tenedores( id_propio ); //   es un tenedor
-   }
-   else
-   {
+	funcion_tenedores( id_propio ); //   es un tenedor
+    }
+  else
+    {
       if ( id_propio == 0 ) // solo el primero escribe error, indep. del rol
-      { cout << "el número de procesos esperados es:    " << num_procesos << endl
-             << "el número de procesos en ejecución es: " << num_procesos_actual << endl
-             << "(programa abortado)" << endl ;
-      }
-   }
+	{ cout << "el número de procesos esperados es:    " << num_procesos << endl
+	       << "el número de procesos en ejecución es: " << num_procesos_actual << endl
+	       << "(programa abortado)" << endl ;
+	}
+    }
 
-   MPI_Finalize( );
-   return 0;
+  MPI_Finalize( );
+  return 0;
 }
 
 // ---------------------------------------------------------------------
